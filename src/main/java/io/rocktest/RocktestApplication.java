ab38b18faac25dae49a349b33066d03a35beb79d
amdmdi <42283870+amdmdi@users.noreply.github.com>
2021-01-24 09:54:25 +0000

    Indent using spaces to mach other files

--- src/main/java/io/rocktest/RocktestApplication.java
+++ src/main/java/io/rocktest/RocktestApplication.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -16,61 +15,59 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RocktestApplication
-		implements CommandLineRunner {
+        implements CommandLineRunner {
 
-	@Autowired
-	private Scenario scenario;
-	
-	@Value("${topic.name}")
-	private String topicName;
+    @Autowired
+    private Scenario scenario;
 
-	@Value("${topic.partitions-num}")
-	private Integer partitions;
+    @Value("${topic.name}")
+    private String topicName;
 
-	@Value("${topic.replication-factor}")
-	private short replicationFactor;
+    @Value("${topic.partitions-num}")
+    private Integer partitions;
+
+    @Value("${topic.replication-factor}")
+    private short replicationFactor;
 
 
-	private static Logger LOG = LoggerFactory
-			.getLogger(RocktestApplication.class);
+    private static Logger LOG = LoggerFactory
+            .getLogger(RocktestApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(RocktestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RocktestApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws IOException, InterruptedException {
-		if(args.length>=1) {
+    @Override
+    public void run(String... args) throws IOException, InterruptedException {
+        if(args.length>=1) {
 
-			File parent =new File(args[0]).getParentFile();
-			String dir=".";
+            File parent =new File(args[0]).getParentFile();
+            String dir=".";
 
-			if(parent != null) {
-				dir=parent.getName();
-			}
+            if(parent != null) {
+                dir=parent.getName();
+            }
 
-			ArrayList<String> stack=new ArrayList<>();
-			stack.add(new File(args[0]).getName().replace(".yaml",""));
+            ArrayList<String> stack=new ArrayList<>();
+            stack.add(new File(args[0]).getName().replace(".yaml",""));
 
-			Map<String, List<Map<String,Object>>> functions = new HashMap<>();
+            // HashMap for each instance of modules
+            scenario.setModuleInstances(new HashMap<>());
 
-			// HashMap for each instance of modules
-			scenario.setModuleInstances(new HashMap<>());
+            Map<String,Object> glob=new HashMap<>();
 
-			Map<String,Object> glob=new HashMap<>();
-
-			String err=scenario.main(args[0],dir,new HashMap<String, Map<String,Object>>(),stack,glob);
-			if(err!=null) {
-				LOG.error("Error : {}",err);
-				System.exit(1);
-			} else {
-				LOG.info("Scenario SUCCESS");
-				System.exit(0);
-			}
-		} else {
-			LOG.error("1 argument missing (scenario)");
-		}
-	}
+            String err=scenario.main(args[0],dir,new HashMap<String, Map<String,Object>>(),stack,glob);
+            if(err!=null) {
+                LOG.error("Error : {}",err);
+                System.exit(1);
+            } else {
+                LOG.info("Scenario SUCCESS");
+                System.exit(0);
+            }
+        } else {
+            LOG.error("1 argument missing (scenario)");
+        }
+    }
 }
